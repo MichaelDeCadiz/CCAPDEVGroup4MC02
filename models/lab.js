1bcf6e36ef5a5a3ba19859d52bf8d69adb5da5bf
jvtorress <104366917+jvtorress@users.noreply.github.com>
2025-07-14 22:42:33 +0800

    Update lab.js

--- models/lab.js
+++ models/lab.js
@@ -1,23 +1,10 @@
 const mongoose = require('mongoose');
-const Lab = require('./Lab'); 
 
-mongoose.connect('mongodb://localhost:27017/labreservationDB');
+const labSchema = new mongoose.Schema({
+  name: String,
+  rows: Number,
+  columns: Number
+});
 
-const labs = [
-  { name: "CL1", rows: 4, columns: 5 },
-  { name: "CL2", rows: 5, columns: 6 },
-  { name: "CL3", rows: 3, columns: 4 },
-  { name: "CL4", rows: 6, columns: 5 },
-  { name: "CL5", rows: 4, columns: 3 }
-];
+module.exports = mongoose.model('Lab', labSchema);
 
-
-Lab.insertMany(labs)
-  .then(() => {
-    console.log("5 sample labs inserted successfully!");
-    mongoose.connection.close();
-  })
-  .catch(err => {
-    console.error("Error inserting labs:", err);
-    mongoose.connection.close();
-  });
